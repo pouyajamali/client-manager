4425dcfa7e8e0210e37e29cd24e0f6500d9d4eb2
Parham Shagerdmootab <psm1379@gmail.com>
2024-07-17 16:39:21 -0700

    Login Page created

---
+++ FE/src/Pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+import "./Login.scss"
+
+export default function Login() {
+    
+
+    return <div className="LoginContainer">
+
+    </div>
+}
--- FE/src/index.js
+++ FE/src/index.js
@@ -1,25 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation  } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import ClientsList from "./Pages/ClientsList/ClientsList";
 import "./index.scss";
 import ClientDetails from "./Pages/ClientDetails/ClientDetails";
 import CreateNewDocument from "./Pages/Document/CreateNewDocument/CreateNewDocument";
+import Login from "./Pages/Login/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function RouteWrapper() {
+  const location = useLocation();
+  const isLoginPage  = location.pathname === "/";
+  return (
+    <>
+      {!isLoginPage && <Header />}
+      <div className={`MainContainer ${isLoginPage ? 'LoginPage' : ''}`}>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/clients" element={<ClientsList />} />
+          <Route exact path="/:clientId" element={<ClientDetails />} />
+          <Route
+            exact
+            path="/document/create"
+            element={<CreateNewDocument />}
+          />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
 root.render(
   <React.StrictMode>
     <Router>
-      <Header />
-      <div className="MainContainer">
-        <Routes>
-          <Route exact path="/" element={<ClientsList />} />
-          <Route exact path="/:clientId" element={<ClientDetails />} />
-          <Route exact path="/document/create" element={<CreateNewDocument />} />
-        </Routes>
-      </div>
+      <RouteWrapper />
     </Router>
   </React.StrictMode>
 );
